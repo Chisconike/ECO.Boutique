4db627a3aa4e9b77a21f8c50f464f6fff98e3589
Chisconike <chinedue856@gmail.com>
2023-11-02 11:06:23 +0000

    first commit

---
+++ assest/js/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+  });
+});
+
